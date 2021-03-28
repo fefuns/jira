3f67a8f7d0a639d917344ffe714ebe2c0efcc210
何智 <hezhi@yunzujia.com>
2021-03-29 01:38:15 +0800

    feat: 通过中间件赋予json-server自定义api风格的能力

---
+++ __json_server_mock__/middleware.js
@@ -0,0 +1,16 @@
+module.export = (req, res, next) =>{
+  if(req.method==='POST' && req.path==='/login') {
+    if(req.body.username === 'admin' && req.body.password === '123456') {
+      return res.status(200).json({
+        user: {
+          token: '123'
+        }
+      })
+    } else {
+      return res.status(400).json({
+        message: '用户名或密码错误'
+      })
+    }
+  }
+  next()
+}
