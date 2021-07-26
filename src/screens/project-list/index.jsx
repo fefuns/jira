a2f925fddf85ca9df93c5d4422890bd5ed911f8d
何智 <1321120469@qq.com>
2021-07-27 00:03:05 +0800

    feat: 自定义hooks

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { List } from './list'
 import { SearchPanel } from './search-panel'
 import { stringify } from 'qs';
-import { cleanObject } from 'utils';
+import { cleanObject, useDebounce, useMount } from 'utils';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
@@ -12,20 +12,21 @@ export const ProjectListScreen = () => {
   })
   const [list, setList] = useState([])
   const [users, setUsers] = useState([]);
+  const debouncedParam = useDebounce(param, 500);
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${stringify(cleanObject(param))}`).then(async res => {
+    fetch(`${apiUrl}/projects?${stringify(cleanObject(debouncedParam))}`).then(async res => {
       if (res.ok) {
         setList(await res.json())
       }
     })
-  }, [param])
-  useEffect(() => {
+  }, [debouncedParam])
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async res => {
       if (res.ok) {
         setUsers(await res.json())
       }
     })
-  }, []);
+  });
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam} />
     <List list={list} users={users} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 // 在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (obj) => {
@@ -9,4 +11,20 @@ export const cleanObject = (obj) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再执行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
 };
