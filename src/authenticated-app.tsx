e8836712098f6097d44dbe279c8ad74687c5b6de
fefuns <1321120469@qq.com>
2021-07-28 01:37:46 +0800

    style: 改写成antd元素

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,11 +1,11 @@
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
-
+import { Button } from "antd";
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <div>
-      <button onClick={logout}>登出</button>
+      <Button onClick={logout}>登出</Button>
       <ProjectListScreen />
     </div>
   );
